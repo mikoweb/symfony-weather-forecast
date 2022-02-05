e12fd3718c03e3f98a8f73d3a55aa32c8f3b5029
Rafał Mikołajun <root@rmweb.pl>
2022-02-05 18:59:26 +0100

    add "Randomize" DataSource

---
+++ src/Weather/DataSource/Randomize/GetForecastQuery.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Weather\DataSource\Randomize;
+
+use App\Weather\CoordInterface;
+use App\Weather\DataSource\GetForecastQueryInterface;
+use App\Weather\DataSource\OpenWeather\GetForecastQuery as BaseGetForecastQuery;
+use App\Weather\Forecast;
+use App\Weather\ForecastInterface;
+use App\Weather\Wind;
+
+final class GetForecastQuery implements GetForecastQueryInterface
+{
+    private BaseGetForecastQuery $query;
+
+    public function __construct(BaseGetForecastQuery $query)
+    {
+        $this->query = $query;
+    }
+
+    public function get(CoordInterface $coord): ForecastInterface
+    {
+        $base = $this->query->get($coord);
+
+        return new Forecast(
+            coord: clone $coord,
+            temperature: $this->rand($base->getTemperature(), 4),
+            pressure: $this->rand($base->getPressure(), 300, 0),
+            humidity: $this->rand($base->getHumidity(), 20, 0),
+            cloudiness: $this->rand($base->getCloudiness(), 20, 0),
+            wind: new Wind(
+                speed: $this->rand($base->getWind()->getSpeed(), 10, 0),
+                deg: $this->rand($base->getWind()->getDeg(), 30),
+            )
+        );
+    }
+
+    private function rand(int $value, int $range, ?int $min = null): int
+    {
+        return rand($min ?? $value - $range, $value + $range);
+    }
+}
