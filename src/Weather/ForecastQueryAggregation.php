b521a8e4683ce0a65e0fd5a2521a64af4958c40b
Rafał Mikołajun <root@rmweb.pl>
2022-02-05 21:02:25 +0100

    query aggregation

---
+++ src/Weather/ForecastQueryAggregation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Weather;
+
+use App\Weather\DataSource\GetForecastQueryCollection;
+use App\Weather\Reducer\ForecastReducerMap;
+use App\Weather\Reducer\ReducerInterface;
+
+class ForecastQueryAggregation
+{
+    private GetForecastQueryCollection $collection;
+
+    public function __construct(GetForecastQueryCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    public function aggregate(CoordInterface $coord): ForecastInterface
+    {
+        $forecasts = $this->collection->getResult($coord);
+
+        return new Forecast(
+            coord: clone $coord,
+            temperature: $this->getReducer('temperature')->reduce($forecasts->getColumn('temperature')),
+            pressure: $this->getReducer('pressure')->reduce($forecasts->getColumn('pressure')),
+            humidity: $this->getReducer('humidity')->reduce($forecasts->getColumn('humidity')),
+            cloudiness: $this->getReducer('cloudiness')->reduce($forecasts->getColumn('cloudiness')),
+            wind: new Wind(
+                speed: $this->getReducer('wind_speed')->reduce($forecasts->getColumn('speed', 'wind')),
+                deg: $this->getReducer('wind_deg')->reduce($forecasts->getColumn('deg', 'wind'))
+            )
+        );
+    }
+
+    private function getReducer(string $context): ReducerInterface
+    {
+        $reducerClass = (new ForecastReducerMap())->getReducerClass($context);
+
+        return new $reducerClass;
+    }
+}
---
+++ src/Weather/ForecastQueryAggregationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Weather;
+
+use App\Weather\DataSource\GetForecastQueryCollection;
+use App\Weather\DataSource\OpenWeather\GetForecastQuery as OpenWeatherQuery;
+use App\Weather\DataSource\Randomize\GetForecastQuery as RandomizeQuery;
+
+final class ForecastQueryAggregationFactory
+{
+    private OpenWeatherQuery $query1;
+    private RandomizeQuery $query2;
+
+    public function __construct(OpenWeatherQuery $query1, RandomizeQuery $query2)
+    {
+        $this->query1 = $query1;
+        $this->query2 = $query2;
+    }
+
+    public function create(): ForecastQueryAggregation
+    {
+        return new ForecastQueryAggregation(new GetForecastQueryCollection([
+            $this->query1,
+            $this->query2
+        ]));
+    }
+}
